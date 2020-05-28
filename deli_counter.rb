eaa37db6e261a13a4c93e999e09b232249af1c4b
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 17:23:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,13 @@
 my_array = ["erdo" , "semra"]
 
 def line(array)
- if 0 < array.size
+ if 0 < array.size 
+  puts "The line is currently: "
    array.each.with_index do |name, index|
-  puts "The line is currently:" "#{index + 1}. #{name}"
+  print  "#{index + 1}. #{name} "
 end
   
   else puts  "The line is currently empty."
 end
 end
+
