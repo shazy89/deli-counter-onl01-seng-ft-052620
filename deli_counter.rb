60895c4a45523e1c224061e7c9b6623c9d213768
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 16:52:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(array)
  if 0 < array.size
-   return "The line is currently:#{array} "
+   return "The line is currently:#{array,} "
   else puts  "The line is currently empty."
 end
