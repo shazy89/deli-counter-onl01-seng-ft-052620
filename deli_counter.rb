424cdf0a6f8f203dd38d6bdb57d69fc07c1b4963
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 17:15:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
 # Write your code here.
 def line(array)
  if 0 < array.size
-   array.map.each_with_index |item, index|
-   return "The line is currently:#{index +1} #{item} "
+   aarray.each.with_index do |name, index|
+  puts "#{index + 1}. #{name}"
+end
+  
   else puts  "The line is currently empty."
 end
