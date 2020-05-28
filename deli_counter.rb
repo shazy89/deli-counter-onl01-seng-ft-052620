ddfb1bd9d831e6a9fcefff1735e2c5fbe98b9b7b
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 18:34:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
   puts  "Welcome, #{name}. You are number #{katz_deli.size } in line."
 end
 
-def now_serving(array)
+def now_serving(array , name)
   if array.size == 0 
     return puts "There is nobody waiting to be served!"
   else 
